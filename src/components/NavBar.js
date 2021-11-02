8734adcf29f38d663b28736efdb02ebb6ad2731c
Pema Lama <pema.srp72@gmail.com>
2021-11-02 16:54:52 -0400

    commit tree 4f016fe2d269bb0bfaac04a2cba050fd817291e5

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
+
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+ const links = ["home", "about", "projects"];
+ const mappedLinks = links.map((link,index) => <a key = {index} href={`#${link}`} > {link} </a>);
+  return <nav> 
+   {mappedLinks}
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({name, about, technologies }) {
+  console.log(technologies);
+  const techList = technologies.map((tech,index) => <span key ={index} >{tech} </span>);
+  console.log(techList)
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+            {techList}
       </div>
     </div>
   );
